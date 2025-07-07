56d22e34503420c8fbbf1a36ca07966b78334712
kushbharti <try.kushbharti@gmail.com>
2025-07-07 14:12:15 +0530

    Demo Api Created

--- Rest_API/API/admin.py
+++ Rest_API/API/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Company,Employee
 
 # Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=('name','location','types')
+    
+    
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name','email','position')
+    list_filter = ('company',)
+
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmployeeAdmin)
--- Rest_API/API/models.py
+++ Rest_API/API/models.py
@@ -7,10 +7,31 @@ class Company(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
     about = models.TextField()
-    types= models.CharField(max_length=100,choices=(('IT','IT'),('Non IT','Non IT'),('Mobile Phones','Mobile Phones')))
+    types= models.CharField(max_length=100,choices=(('IT','IT'),
+                                                    ('Non IT','Non IT'),
+                                                    ('Mobile Phones','Mobile Phones')
+                                                    ))
     added_time =models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
     
     
     def __str__(self):
         return self.name +' -- '+ self.location
+    
+    
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email =models.EmailField(unique=True)
+    address = models.CharField(max_length=100)
+    phone = models.IntegerField(default=0,null=True)
+    about = models.TextField()
+    position = models.CharField(max_length=50,choices=(('Manager','Manager'),
+                                                       ('Software Developer','SD'),
+                                                       ('Project Leader','PL')
+                                                       ))
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name
